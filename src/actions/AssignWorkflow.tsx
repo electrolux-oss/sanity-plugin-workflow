cef649bed593e0d9b6981975acceb2dd8c005ad1
Philip Åkerfeldt <philip.akerfeldt@swace.se>
2025-12-15 11:18:00 +0100

    feat: update import formatting in AssignWorkflow

--- src/actions/AssignWorkflow.tsx
+++ src/actions/AssignWorkflow.tsx
@@ -1,28 +1,24 @@
+import type { DocumentActionProps } from 'sanity'
 import { useState } from 'react'
+import { useProjectUsers } from 'sanity-plugin-utils'
 
 import { UsersIcon } from '@sanity/icons'
 
 import UserAssignment from '../components/UserAssignment'
 import { useWorkflowContext } from '../components/WorkflowContext'
 import { API_VERSION } from '../constants'
-import { useProjectUsers } from '../hooks/useUsers'
-
-import type { DocumentActionProps } from 'sanity'
 
 export function AssignWorkflow(props: DocumentActionProps) {
   const { id } = props
   const { metadata, loading, error } = useWorkflowContext(id)
   const [isDialogOpen, setDialogOpen] = useState(false)
   const userList = useProjectUsers({ apiVersion: API_VERSION })
-
   if (error) {
     console.error(error)
   }
-
   if (!metadata) {
     return null
   }
-
   return {
     icon: UsersIcon,
     type: 'dialog',
