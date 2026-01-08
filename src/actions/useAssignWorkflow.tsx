93bebf3a225ccb4509cdd08220e1b75f3e93ee33
Philip Åkerfeldt <philip.akerfeldt@swace.se>
2026-01-08 15:32:30 +0100

    chore: use custom hook instead of sanity plugin utils in useAssignWorkflow

--- src/actions/useAssignWorkflow.tsx
+++ src/actions/useAssignWorkflow.tsx
@@ -1,23 +1,26 @@
 import type { DocumentActionProps, DocumentActionDescription } from 'sanity'
 import { useState } from 'react'
-import { useProjectUsers } from 'sanity-plugin-utils'
 
 import { UsersIcon } from '@sanity/icons'
 
 import UserAssignment from '../components/UserAssignment'
 import { useWorkflowContext } from '../components/WorkflowContext'
 import { API_VERSION } from '../constants'
+import { useProjectUsers } from '../hooks/useUsers'
 
 export function useAssignWorkflow({ id }: DocumentActionProps): DocumentActionDescription | null {
   const { metadata, loading, error } = useWorkflowContext(id)
   const [isDialogOpen, setDialogOpen] = useState(false)
   const userList = useProjectUsers({ apiVersion: API_VERSION })
+
   if (error) {
     console.error(error)
   }
+
   if (!metadata) {
     return null
   }
+
   return {
     icon: UsersIcon,
     disabled: !metadata || loading || Boolean(error),
@@ -29,6 +32,7 @@ export function useAssignWorkflow({ id }: DocumentActionProps): DocumentActionDe
         setDialogOpen(false)
       },
       content: (
+        // <div>Testing</div>
         <UserAssignment
           userList={userList}
           assignees={metadata?.assignees?.length > 0 ? metadata.assignees : []}
